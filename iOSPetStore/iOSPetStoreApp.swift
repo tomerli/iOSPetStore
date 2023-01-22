69ab3bd25092bceb9fb274c950b13d4dc5707359
Tomer <tomerli@gmail.com>
2023-01-22 15:57:25 +0200

    Initial Commit

---
+++ iOSPetStore/iOSPetStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSPetStoreApp.swift
+//  iOSPetStore
+//
+//  Created by Tomer Lihovetsky on 22/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct iOSPetStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
