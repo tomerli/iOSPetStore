7f8213e66b304f5bca7946a2744d8a974920d69c
Tomer <tomerli@gmail.com>
2023-05-31 11:29:45 +0300

    1

--- iOSPetStore/CartView.swift
+++ iOSPetStore/CartView.swift
@@ -43,7 +43,7 @@ struct CartView: View {
                 
             }
             else {
-                Text("Your cart is empty...")
+                Text("Your cart is empty.....")
             }
             
         }
